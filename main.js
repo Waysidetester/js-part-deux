89433672777e8a977b0daa965da36ed9311ea6e2
WaysideTester <dylanmurray2119@gmail.com>
2018-08-27 18:56:32 -0500

    Stopping point in example

--- main.js
+++ main.js
@@ -1,6 +1,36 @@
-const printDom = (stringToPrint, divID) => {
+;const printDom = (stringToPrint, divID) => {
     const selectedDiv = document.getElementById(divID);
     selectedDiv.innerHTML += `${stringToPrint}`;
 };
 
-console.log('i work');
+const age = 26;
+
+if (age >= 21){
+    console.log('Come on in');
+} else {
+    console.log('Get out of here');
+}
+
+//let authenticated === false;
+
+
+
+const a = 1;
+const b = '1';
+
+a === b; //false
+a == b; //true
+
+a !== b; //true
+a!= b; //false
+
+const favoriteColor = 'blue';
+const favoriteAnimal = 'bear';
+
+if (favoriteAnimal === 'bear' && favoriteColor === 'red'){
+    console.log('Welcome to the club');
+} else if (favoriteAnimal === 'cat' || favoriteColor === 'blue') {
+    console.log('$500 to join the club');
+} else {
+    console.log('Go away');
+}
